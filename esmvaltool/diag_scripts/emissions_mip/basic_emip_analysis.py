14aa8d834ab0a73ed847817e11db056d9247c887
Smith, Steven J (PNNL-JGCRI) <ssmith@pnnl.gov>
2020-06-19 16:42:58 -0400

    Rename and reconfigure scripts so absolute and diff run together. Add land_only recipe.

--- esmvaltool/diag_scripts/emissions_mip/basic_emip_analysis.py
+++ esmvaltool/diag_scripts/emissions_mip/basic_emip_analysis.py
@@ -1,8 +1,10 @@
 """
-initial_analysis-giss.py
+basic_analysis.py
 
-Emissions-MIP GISS inititial analysis diagnostic script. Creates a plot of the annual & area average
-of a variable from various GISS CESM configurations. One plot is created per variable.
+Emissions-MIP initial analysis diagnostic script. Creates a plot of the annual, area average
+of a variable from various configurations. One plot is created per variable.
+
+Does the same for differences between reference and perturbation cases.
 
 Matt Nicholson
 23 April 2020
@@ -16,7 +18,7 @@ from esmvalcore.preprocessor import area_statistics
 
 import common_emip_funcs
 from esm_variable import ESMVariable
-    
+
 
 def main(cfg):
     """
@@ -39,28 +41,46 @@ def main(cfg):
       holding all variable metadata.
     * Since the preprocessor extracts the 1000 hPa level data,
       the cube's data will have shape (36, 180, 360) corresponding
-      to time (in months), latitude, longitude. 
+      to time (in months), latitude, longitude.
     """
+    # First produce plot for each variable
+
     # Plot configuration dictionary.
     plt_config = {'ggplot'  : True,
                   'out_dir' : cfg['plot_dir'],
-                  'plt_name': 'time_series-initial_analysis-giss-{}.pdf',
+                  'plt_name': 'time_series-{}.pdf',
                   'time_interval': 'annual',
-                  'title'   : 'Annual Area Average - {}',
+                  'title'   : 'Annual Average - {}',
                   'write_data': True
                   }
     file_dict = group_metadata(cfg['input_data'].values(), 'dataset')
-    # Get a dictionary keyed on variable name where the value is a list of 
-    # variable metadata dict from the various model configs.    
+    # Get a dictionary keyed on variable name where the value is a list of
+    # variable metadata dict from the various model configs.
     var_groups = common_emip_funcs.group_meta_by_var(file_dict)
-    
+
     # Iterate over the variable dictionary and process each varible one-by-one.
     for esm_var, dict_list in var_groups.items():
         # Get list of ESMVariable objects.
         var_list = [ESMVariable(var_dict).get_area_statistic('mean') for var_dict in dict_list]
         common_emip_funcs.plot_timeseries(var_list, plt_config)
-            
- 
+
+    # Now produce plots of differences between ref and perturbation for each variable
+
+    # New plot configuration dictionary.
+    plt_config = {'ggplot'  : True,
+                  'out_dir' : cfg['plot_dir'],
+                  'plt_name': 'time_series-difference-{}.pdf',
+                  'time_interval': 'annual',
+                  'title'   : 'Annual Average Difference - {}',
+                  'write_data': True
+                  }
+
+    # Iterate over the variable dictionary and process each varible one-by-one.
+    for esm_var, dict_list in var_groups.items():
+        # Get list of ESMVariable objects.
+        var_list = [ESMVariable(var_dict).get_area_statistic('mean') for var_dict in dict_list]
+        common_emip_funcs.plot_timeseries_diff(var_list, plt_config)
+
 if __name__ == '__main__':
     # always use run_diagnostic() to get the config (the preprocessor
     # nested dictionary holding all the needed information)
